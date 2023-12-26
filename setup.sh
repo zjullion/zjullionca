90d8496933e55a1a073555761b185b082e78e755
zjullion <zach.jullion@serpensborealis.com>
2023-12-26 15:48:58 -0700

    Fix / improve setup / deploy process

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Note: we don't install aws-cli, as it already comes with Ubuntu
+# install aws-cli
+apt-get install -y awscli
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -9,14 +10,10 @@ export NVM_DIR="$HOME/.nvm"
 
 # install node
 nvm install "$(cat ./.nvmrc)"
-nvm alias default "$(cat ./.nvmrc)"
+nvm use
 
 # install pnpm
 npm install -g pnpm@8
-
-# install global dependencies
-npm install -g ts-node@10
-npm install -g aws-cdk@2.115.0
 
 # install all dependencies
 pnpm install:all
