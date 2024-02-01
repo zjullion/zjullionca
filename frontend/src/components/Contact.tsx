0dae46e42fa88dcdd251d528a489d381774f87bd
zjullion <thezarjio@gmail.com>
2024-02-01 09:01:23 -0700

    Finish contact page

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -6,7 +6,7 @@ import { FlexChild, FlexDiv, SafeLink, Text, Title } from './base'
 const INPUT_CSS = css`
   border-radius: 2px;
   padding: 2px;
-  margin: 3px 0 12px 5px;
+  margin: 3px 0 18px 5px;
   font-size: 0.95em;
 `
 
@@ -19,12 +19,46 @@ const StyledTextArea = styled.textarea`
   ${INPUT_CSS}
 `
 
+const ErrorMessage = styled(Text)`
+  color: red;
+  margin: 0 0 0 5px;
+`
+
+const StyledButton = styled.button`
+  font-size: 0.9em;
+`
+
 const EMAIL_REGEX = /[^@]+@[^@]+\.[^@]+/
 
 export const Contact: FunctionComponent = () => {
   const [name, setName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [message, setMessage] = useState<string>('')
+
+  const [nameError, setNameError] = useState<string | undefined>()
+  const [emailError, setEmailError] = useState<string | undefined>()
+  const [messageError, setMessageError] = useState<string | undefined>()
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
+  const submit = async () => {
+    setIsSubmitting(true)
+    if (name.length === 0) {
+      setNameError('Name required.')
+    }
+    if (email.length === 0) {
+      setEmailError('Email required.')
+    } else if (!EMAIL_REGEX.test(email)) {
+      setEmailError('Invalid email.')
+    }
+    if (message.length === 0) {
+      setMessageError('Message required.')
+    }
+    if (nameError == null && emailError == null && messageError == null) {
+      console.log('TODO')
+    }
+    setIsSubmitting(false)
+  }
 
   return (
     <FlexDiv>
@@ -42,35 +76,50 @@ export const Contact: FunctionComponent = () => {
         </Text>
       </FlexChild>
       <FlexChild>
-        <Title>Message Form</Title>
+        <Title>Contact Form</Title>
         <label>Name</label>
         <br />
+        {nameError != null ? <ErrorMessage>{nameError}</ErrorMessage> : undefined}
         <StyledInput
           maxLength={30}
-          onChange={(event) => setName(event?.target?.value)}
+          onChange={(event) => {
+            setName(event?.target?.value)
+            setNameError(undefined)
+          }}
           type="text"
           value={name}
         />
         <br />
         <label>Email</label>
         <br />
+        {emailError != null ? <ErrorMessage>{emailError}</ErrorMessage> : undefined}
         <StyledInput
           maxLength={60}
-          onChange={(event) => setEmail(event?.target?.value)}
+          onChange={(event) => {
+            setEmail(event?.target?.value)
+            setEmailError(undefined)
+          }}
           type="text"
           value={email}
         />
         <br />
         <label>Message</label>
         <br />
+        {messageError != null ? <ErrorMessage>{messageError}</ErrorMessage> : undefined}
         <StyledTextArea
           cols={35}
           maxLength={500}
-          onChange={(event) => setMessage(event?.target?.value)}
+          onChange={(event) => {
+            setMessage(event?.target?.value)
+            setMessageError(undefined)
+          }}
           rows={5}
           value={message}
         />
         <br />
+        <StyledButton disabled={isSubmitting} onClick={submit}>
+          Submit
+        </StyledButton>
       </FlexChild>
     </FlexDiv>
   )
